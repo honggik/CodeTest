fb8c212192af6abad27e94bf2a04edfdeb41dc6b
anto4531 <anto4531@naver.com>
2022-01-31 23:08:23 +0900

    수정

---
+++ ConsoleTest0128/NotNUmber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest0128
+{
+	//없는 숫자	더하기
+	class NotNUmber
+	{
+		static void Solution1()
+		{
+			//int[] numbers = { 1, 2, 3, 4, 6, 7, 8, 0 };
+			Console.Write("숫자입력: ");
+			int num = Convert.ToInt32(Console.ReadLine());
+
+			int[] numbers = new int[num];
+
+			int answer = 45;
+
+			foreach (int number in numbers)
+			{
+				answer -= number;
+			}
+
+			Console.WriteLine(answer);
+		}
+		static void Main()
+		{
+			Solution1();
+		}
+	}
+}
---
+++ ConsoleTest0128/inner_product.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest0128
+{
+	class inner_product
+	{
+		//내적
+		static int Inner()
+		{
+			int[] a = { 1, 2, 3, 4 };
+			int[] b = { -3, -1, 0, 2 };
+			int result = 0;
+			for (int i = 0; i < a.Length; i++)
+			{
+				result = result + a[i] * b[i];
+			}
+			return result;
+		}
+
+		//음양 더하기 
+
+		static int absolute()
+		{
+			int []abs = { 4, 7, 12 };
+			bool[] signs = { true, false, true };
+			int result = 0;
+
+			if (abs.Length == signs.Length)
+			{
+				for (int i = 0; i<abs.Length; i++)
+				{
+					if (signs[i] == false)
+					{
+						abs[i] = abs[i] - (abs[i] * 2);
+					}
+					result = result + abs[i];
+				}
+			}
+
+			return result;
+			
+		}
+		static void Main()
+		{
+			int result = Inner();
+			Console.WriteLine(result);
+
+			int result1 = absolute();
+			Console.WriteLine(result1);
+		}
+	}
+}
