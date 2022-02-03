413b9e2131f448e09cc6876f262c2dbc0e873a7b
anto4531 <anto4531@naver.com>
2022-02-03 19:48:56 +0900

    알고리즘 문제 풀이

---
+++ ConsoleTest0128/KIM.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest0128
+{
+	class KIM
+	{
+		//서울에서 김서방 찾기
+		static void Solution()
+		{
+			string answer = "";
+			string [] seoul = new string []{ "Lee","Kim" };
+				
+			for (int i = 0; i < seoul.Length; i++)
+			{
+				if (seoul[i] == "Kim")
+				{
+					answer = $"김서방은{i}에 있다.";
+				}
+			}
+			Console.WriteLine(answer);
+		}
+
+		static void Main()
+		{
+			Solution();
+		}
+	}
+}
---
+++ ConsoleTest0128/string_false_true.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest0128
+{
+	class string_false_true
+	{
+		static void Solution()
+		{
+			bool bconvert = false;
+
+			int k;
+			for (; ; )
+			{
+				Console.Write("입력: ");
+				string s = Console.ReadLine();
+				if (s.Length == 4 || s.Length == 6)
+				{
+					bconvert = int.TryParse(s, out k);
+					if (bconvert)
+					{
+						Console.WriteLine(bconvert);
+						//break;
+					}
+					else
+					{
+						Console.WriteLine(bconvert.ToString());
+					}
+				}
+				else
+				{
+					Console.Write("message");
+				}
+			}
+		}
+
+		static void Main()
+		{
+			Solution();
+		}
+
+		
+	}
+}
