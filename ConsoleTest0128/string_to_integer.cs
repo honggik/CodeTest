3b58f24c6d81e0265ca230ac397f4feb51d555c3
anto4531 <anto4531@naver.com>
2022-02-04 08:49:48 +0900

    문자열을 정수로

---
+++ ConsoleTest0128/string_to_integer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest0128
+{
+	class string_to_integer
+	{
+		//문자열을 정수로 
+		static void Solution1()
+		{
+
+			bool b = false;
+
+			int inputNum;
+
+			for (; ; )
+			{
+				Console.Write("입력");
+				string s = Console.ReadLine();
+
+				b = int.TryParse(s, out inputNum);
+				if (b)
+				{
+					Console.WriteLine(inputNum);
+				}
+				else
+				{
+					Console.WriteLine(s);
+				}
+			}
+		}
+		static void Main()
+		{
+			Solution1();
+		}
+	}
+}
