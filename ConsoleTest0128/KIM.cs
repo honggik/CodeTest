137d255972caf3c1c0291d381e6ec8bab1d4e621
anto4531 <anto4531@naver.com>
2022-02-03 20:03:47 +0900

    수박수박수

--- ConsoleTest0128/KIM.cs
+++ ConsoleTest0128/KIM.cs
@@ -33,11 +33,33 @@ namespace ConsoleTest0128
 
 			Console.WriteLine($"김서방은{index}에 있다.");
 		}
+		static void WaterMelon()
+		{
+			//수박수박수
+			int n = 4;
+			string answer = string.Empty;
+
+			for (int i = 1; i <= n; i++)
+			{
+				if (i % 2 == 0)
+				{
+					answer += "박";
+				}
+				else
+				{
+					answer += "수";
+				}
+			}
+			Console.WriteLine();
+			Console.Write(answer);
+
+		}
 
 		static void Main()
 		{
 			Solution();
 			Exam();
+			WaterMelon();
 		}
 	}
 }
