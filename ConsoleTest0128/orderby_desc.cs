6dfb2a12078a531cf27d830920d1f059acc5c05a
anto4531 <anto4531@naver.com>
2022-02-03 19:28:25 +0900

    람다식으로도 odrederbyDesending으로 구현 할 수 있다.

---
+++ ConsoleTest0128/orderby_desc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest0128
+{
+	class orderby_desc
+	{
+		static string s = "Zbcdefg";
+		static void Solution()
+		{
+			
+
+			//char에 배열을 넣는다
+			char[] temp = s.ToCharArray();
+			//오름차순으로 정렬
+			Array.Sort(temp);
+			//내림차순으로 정렬
+			Array.Reverse(temp);
+
+			
+			string answer = new string(temp);
+			Console.WriteLine(answer);
+
+		}
+
+		static void Exam()
+		{
+			//람다식으로 구현
+			string answer = new String(s.ToCharArray().OrderByDescending(x => x).ToArray());
+			Console.WriteLine(answer);
+		}
+		static void Main()
+		{
+			Solution();
+			Exam();
+		}
+	}
+}
