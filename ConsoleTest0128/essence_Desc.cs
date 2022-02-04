b5b7ac9927376fe5f24656a95b2123c798166583
anto4531 <anto4531@naver.com>
2022-02-04 10:27:44 +0900

    정수를 내림차순으로

---
+++ ConsoleTest0128/essence_Desc.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest0128
+{
+	class essence_Desc
+	{
+		static int n = 118372;
+
+		static void Solution()
+		{
+			char[] c = n.ToString().ToCharArray();
+			long answer = 0;
+
+			Array.Sort(c);
+			Array.Reverse(c);
+
+			answer = Convert.ToInt64(new string(c));
+
+			Console.WriteLine(answer);
+		}
+
+		static void Bubble()
+		{
+			List<int> list = new List<int>();
+			string s = n.ToString();
+
+			//listr에 담는다.
+			//쪼개서 담아가짐
+			foreach (var item in s)
+			{
+				list.Add(int.Parse(item.ToString()));
+			}
+
+			//버블정렬
+			for (int i = 0; i < list.Count; i++)
+			{
+				for (int j = 0; j < i; j++)
+				{
+					if (list[i] > list[j])
+					{
+						int temp = list[i];
+						list[i] = list[j];
+						list[j] = temp;
+					}
+				}
+			}
+
+			string str = string.Empty;
+
+			foreach (var item in list)
+			{
+				str = string.Format("{0}{1}", str, item);
+			}
+			Console.WriteLine(str);
+		}
+
+		static void Main()
+		{
+
+			//Solution();
+			Bubble();
+			
+		}
+	}
+}
