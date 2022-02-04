66fcc700e3f9e66bfb1473b1799c84fa7146d3c7
anto4531 <anto4531@naver.com>
2022-02-04 10:04:17 +0900

    자연수를 뒤집어

---
+++ ConsoleTest0128/flip_NaturalNum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest0128
+{
+	class flip_NaturalNum
+	{
+		//자연수를 뒤집어 
+		static void Solution()
+		{
+			int n = 12345;
+			char[] s = n.ToString().ToCharArray();
+			int[] answer = new int[s.Length];
+
+			for (int i = 0; i < s.Length; i++)
+			{
+				answer[i] = Convert.ToInt32(s[s.Length - i - 1].ToString());
+			}
+			foreach (int item in answer)
+			{
+				Console.WriteLine(item);
+			}
+		}
+		static void Main()
+		{
+				Solution();
+		}
+	}
+}
