71299408e9574d0f4a16448ec5f4d85baca48997
anto4531 <anto4531@naver.com>
2022-02-04 11:51:20 +0900

    재귀 함수 실패

---
+++ ConsoleTest0128/Common divisor_Common_Mutiple.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest0128
+{
+	class Common_divisor_Common_Mutiple
+	{
+		//최대 공약수 , 최소 공배수
+
+		static void Solution(int m,int n)
+		{
+			
+		
+			int[] answer = new int[2];
+			int max = m;
+			int min = n;
+			int a = min;
+
+			while (true)
+			{
+				a = max * min;
+				max = min;
+				if (a  == 0)
+				{
+					break;
+				}
+				min = a;
+			}
+			answer = new int[] { min, n * m / min };
+			Console.WriteLine(answer);
+
+		}
+
+	
+		static int gcd(int n, int m)
+		{
+			if (m == 0)
+			{
+				return n;
+			}
+			else return gcd(m, n % m);
+		}
+
+		static void Main()
+		{
+			int m = 12;
+			int n = 3;
+			Solution(m,n);
+		}
+	}
+}
