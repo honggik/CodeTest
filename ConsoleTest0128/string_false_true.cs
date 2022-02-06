8b7e0f4669c9a121157dfca05576f6631aeefb2a
anto4531 <anto4531@naver.com>
2022-02-06 23:33:33 +0900

    주석

--- ConsoleTest0128/string_false_true.cs
+++ ConsoleTest0128/string_false_true.cs
@@ -17,14 +17,17 @@ namespace ConsoleTest0128
 			{
 				Console.Write("입력: ");
 				string s = Console.ReadLine();
+
 				if (s.Length == 4 || s.Length == 6)
 				{
+					//숫자면 숫자로 출력
 					bconvert = int.TryParse(s, out k);
 					if (bconvert)
 					{
 						Console.WriteLine(bconvert);
 						//break;
 					}
+					//아니면 문자로 출력 
 					else
 					{
 						Console.WriteLine(bconvert.ToString());
