adb7346f3c1bbb71b08437f3e05319528a997bd6
anto4531 <anto4531@naver.com>
2022-02-08 20:22:32 +0900

    2016년 요일구하기 복숩

--- ConsoleTest0128/2016.cs
+++ ConsoleTest0128/2016.cs
@@ -10,10 +10,9 @@ namespace ConsoleTest0128
 	{
 		static void Solution1()
 		{
+			//2016년
 			int a = 5;
 			int b = 24;
-		
-
 
 		}
 		static void Solution2()
@@ -21,8 +20,17 @@ namespace ConsoleTest0128
 			int a = 5;
 			int b = 24;
 			DateTime days = new DateTime(2016, a, b);
-			string day =  days.DayOfWeek.ToString().Substring(0, 3).ToUpper();
+			string day = days.DayOfWeek.ToString().Substring(0, 3).ToUpper();
 			Console.WriteLine(day);
+		}
+
+		static void exam()
+		{
+			//년 월 일 => 오버로드임 년월일 시간 도 할수있음
+			DateTime date = new DateTime(2016, 5, 24);
+			//the만 나오게 하기위해서 요일구하기
+			string day = date.DayOfWeek.ToString().Substring(0, 3).ToString();
+
 		}
 
 		static void Main()
