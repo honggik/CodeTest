4aabe42db52857866ad6b27aa9619f92de5f9fb5
anto4531 <anto4531@naver.com>
2022-02-09 01:23:03 +0900

    행렬의 덧셈 수정

--- ConsoleTest0128/Matrix.cs
+++ ConsoleTest0128/Matrix.cs
@@ -17,11 +17,11 @@ namespace ConsoleTest0128
 		{
 			int[,] arr1 = { { 1, 2 }, { 2, 3 } };
 			int[,] arr2 = { { 3, 4 }, { 5, 6 } };
-			int[,] answer = new int[arr1.Length, arr2.Length];
+			int[,] answer = new int[arr1.GetLength(0), arr1.GetLength(1)];
 			//getLength에 알아오기 
-			for (int i = 0; i < arr1.Length; i++)
+			for (int i = 0; i < arr1.GetLength(0); i++)
 			{
-				for (int j = 0; j < arr2.Length; j++)
+				for (int j = 0; j < arr1.GetLength(1); j++)
 				{
 					answer[i, j] = arr1[i, j] + arr2[i, j];
 				}
