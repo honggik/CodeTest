a630b6c492a080805cf12534694d9fdfe825c41a
anto4531 <anto4531@naver.com>
2022-02-08 20:43:32 +0900

    시저암호 (별표)

--- ConsoleTest0128/Alphabets.cs
+++ ConsoleTest0128/Alphabets.cs
@@ -8,7 +8,7 @@ namespace ConsoleTest0128
 {
 	class Alphabets
 	{
-		//
+		//시저암호
 		static string Solution1()
 		{
 			string answer = "";
@@ -21,7 +21,7 @@ namespace ConsoleTest0128
 			{
 				if (c[i] == ' ') continue;
 
-				//n만큼 문자를 밀어낸다.
+				//n만큼 문자를 밀어내기위해서
 				int temp = Convert.ToInt32(c[i] + n);
 
 				if (c[i] >= 'A' && c[i] <= 'Z')
@@ -81,9 +81,46 @@ namespace ConsoleTest0128
 			}
 			string result = new string(c);
 			Console.WriteLine(result);
-			
+		}
 
-			
+		static void Exam1()
+		{
+			string s = "a B z";
+			int n = 4;
+
+
+			char [] c = s.ToCharArray();
+			for (int i = 0; i < s.Length; i++)
+			{
+				if (c[i] == ' ') continue; // 공백밀기
+				//n만큼 밀어냄
+				int temp = Convert.ToInt32(c[i]) + n;
+
+				//C[I]가 A보다 커야하고 Z보다 작아야함
+				//알파벳 안에 들어와야함
+				if (c[i] >= 'A' && c[i] <= 'Z')
+				{
+					if (temp > 'Z')
+					{
+						temp -= 26;
+					}
+				}
+
+				else
+				{
+					if (temp > 'z')
+					{
+						temp -= 26;
+					}
+				}
+
+				c[i] = Convert.ToChar(temp);
+
+				//string으로 다시 만들기 위해서 
+				string result = new string(c);
+				Console.WriteLine(result);
+
+			}
 		}
 
 		static void Main()
