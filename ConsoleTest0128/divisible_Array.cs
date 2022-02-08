ccbbb491494f2fb1a8600884ba51622838a2c4ba
anto4531 <anto4531@naver.com>
2022-02-08 21:37:08 +0900

    나누어 떨어지는 배열 구하기

--- ConsoleTest0128/divisible_Array.cs
+++ ConsoleTest0128/divisible_Array.cs
@@ -8,6 +8,7 @@ namespace ConsoleTest0128
 {
 	class divisible_Array
 	{
+		//나누어 떨어지는 배열 구하기 
 		static void Solution1()
 		{
 			int[] arr = { 5, 9, 7, 10 };
@@ -39,6 +40,36 @@ namespace ConsoleTest0128
 
 
 		}
+		static void Exam1()
+		{
+			//
+			int[] n = { 5, 9, 8, 10 };
+			int d = 5;
+			List<int> answer = new List<int>();
+			for (int i = 0; i < n.Length; i++)
+			{
+				if (n[i] % d == 0)
+				{
+					//temp는 나누어 떨어지는 n[i]를 담음
+					int temp = n[i];
+
+					//temp를 리스트에 담기
+					answer.Add(temp);
+				}
+				//count 0이면 -1을 넣어주기 
+				if (answer.Count == 0)
+				{
+					answer.Add(-1);
+				}
+			}
+			answer.Sort();
+			foreach (int item in answer)
+			{
+				Console.WriteLine(item);
+			}
+		}
+
+		
 		static void Main()
 		{
 			Solution1();
