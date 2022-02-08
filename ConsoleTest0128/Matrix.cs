b7a7c21ad1b4e86d3c27fd2ce96e23d3b6d7f8c2
anto4531 <anto4531@naver.com>
2022-02-09 01:15:11 +0900

    헹렬의 덧셈

---
+++ ConsoleTest0128/Matrix.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest0128
+{
+	class Matrix
+	{
+		//행렬의 덧셈
+		static void Main()
+		{
+			Solution();
+		}
+		static void Solution()
+		{
+			int[,] arr1 = { { 1, 2 }, { 2, 3 } };
+			int[,] arr2 = { { 3, 4 }, { 5, 6 } };
+			int[,] answer = new int[arr1.Length, arr2.Length];
+			//getLength에 알아오기 
+			for (int i = 0; i < arr1.Length; i++)
+			{
+				for (int j = 0; j < arr2.Length; j++)
+				{
+					answer[i, j] = arr1[i, j] + arr2[i, j];
+				}
+			}
+
+			foreach (int item in answer)
+			{
+				Console.WriteLine(item);
+			}
+		}
+
+	}
+}
---
+++ ConsoleTest0128/x.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest0128
+{
+	class x
+	{
+		static void Main()
+		{
+			Solution();
+		}
+		static void Solution()
+		{
+			int x = 2;
+			int n = 5;
+			List<int> answer = new List<int>();
+
+			for (int i = 1; i <= n; i++)
+			{
+				answer.Add(x);
+				x += 2;
+			}
+
+			foreach (int item in answer)
+			{
+				Console.Write(item+ " ");
+			}
+		}
+
+	}
+}
