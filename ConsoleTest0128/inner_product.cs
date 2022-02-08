fae387a0c83e385b3c09d671e7b9a5e4ccdcc5a3
anto4531 <anto4531@naver.com>
2022-02-08 22:25:14 +0900

    음양더하기,내적

--- ConsoleTest0128/inner_product.cs
+++ ConsoleTest0128/inner_product.cs
@@ -44,7 +44,50 @@ namespace ConsoleTest0128
 			return result;
 			
 		}
-		static void Main()
+
+		//내적구하기
+		static void Exam1()
+		{
+
+			int[] a = { 1, 2, 3, 4 };
+			int[] b = { -3, -1, 0, 2 };
+
+			int answer = 0;
+			if (a.Length == b.Length)
+			{
+				for (int i = 0; i < a.Length; i++)
+				{
+					 answer = answer + (a[i]*b[i]);
+				}
+			}
+			Console.WriteLine(answer);
+		}
+		//음양구하기
+		//sign이 false 이면 
+		// abs수에 -값으로 나오게 하기 
+		// 7-(7*2) 하면됨
+		//그렇게 바꾸고 배열의 합을 구하여라 
+
+		static void Exam2()
+		{
+			int[] abs = { 4, 7, 12 };
+			bool[] signs = { true, false, true };
+			int answer = 0;
+			if (abs.Length == signs.Length)
+			{
+				for (int i = 0; i < abs.Length; i++)
+				{
+					if (signs[i] == false)
+					{
+						abs[i] = abs[i] - abs[i] * 2;
+					}
+					answer = answer + abs[i];
+				}
+			}
+			Console.WriteLine(answer);
+			
+
+			static void Main()
 		{
 			int result = Inner();
 			Console.WriteLine(result);
