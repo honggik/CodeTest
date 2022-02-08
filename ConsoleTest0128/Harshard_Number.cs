d1b1c02cb11a88059c95fa72ec7daae20ce21f5f
anto4531 <anto4531@naver.com>
2022-02-09 00:54:13 +0900

    하샤드수 모바일

---
+++ ConsoleTest0128/Harshard_Number.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest0128
+{
+	public class Harshard_Number
+	{
+		//하샤드 수구하기
+		//x = 18이면 자리숫 덧셈 1+8 = 9 => 18%9 == 0 이면 true
+		static void Main()
+		{
+			Solution();
+		}
+		static void Solution()
+		{
+			int x = 18;
+			int result = 0;
+			bool bCheck = true;
+			string s = x.ToString();
+			for (int i = 0; i < s.Length; i++)
+			{
+				result += Convert.ToInt32(s.Substring(i, 1));
+			
+			}
+			if (x % result == 0)
+			{
+				bCheck = true;
+				Console.WriteLine(bCheck);
+			}
+			else
+			{
+				bCheck = false;
+				Console.WriteLine(bCheck);
+			}
+
+		}
+	}
+}
---
+++ ConsoleTest0128/Mobile.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest0128
+{
+	class Mobile
+	{
+		//모바일 핸드폰의 뒤에 4자리 뺴고 전부다 *
+		static void Main()
+		{
+			Solution2();
+		}
+		static void Solution()
+		{
+			string phone_number = "01050364531";
+
+			string result = string.Empty;
+
+			for (int i = 0; i < phone_number.Length; i++)
+			{
+				if (i < phone_number.Length - 4)
+					result = result + "*";
+				else
+				{
+					result += phone_number.Substring(i, 1);
+				}
+			}
+			Console.WriteLine(result);
+		}
+		static void Solution2()
+		{
+			//문자열 반환은 스트링 빌더가 훨씬 빠르다 
+			//유용 가변타입이라서 
+
+			string phone_number = "01050364531";
+
+			StringBuilder sb = new StringBuilder();
+
+			for (int i = 0; i < phone_number.Length; i++)
+			{
+				if (i < phone_number.Length - 4)
+					sb.Append("*");
+				else
+				{
+					sb.Append(phone_number[i]);
+				}
+			}
+			Console.WriteLine(sb.ToString());
+
+
+		}
+	}
+}
