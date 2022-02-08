343b21a6267e471b4a783259625f52a8c6b274bf
anto4531 <anto4531@naver.com>
2022-02-08 20:55:09 +0900

    예산

--- ConsoleTest0128/Budget.cs
+++ ConsoleTest0128/Budget.cs
@@ -10,19 +10,19 @@ namespace ConsoleTest0128
 	{
 		static void Solution()
 		{
-			int[] d = { 1, 3, 2, 5, 4 };
+			int[] d = { 1, 6, 2, 5, 4 }; //1+2+4, 1+2+5; 1+2+8; 1+4+5; 1+5 // 9가 되는경우가 없음 
 			int budget = 9;
 			int budgetNow = 0;
 			int answer = 0;
 			//작은순서대로 계산하게 만듬
 			//부서 갯수가 최대로 나옴
-			Array.Sort(d); // {1,2,3,4,5};
+			Array.Sort(d); // {1,2,4,5,8};
 			
 			for (int i = 0; i < d.Length; i++)
 			{
 				int smallIndext = i;
 
-				//선택정렬을 하여 가장작은값을 찾음
+			
 
 				
 				//가장 작은값이 예산에 허용된다면 추가
@@ -35,6 +35,7 @@ namespace ConsoleTest0128
 				{
 					break;
 				}
+				
 			}
 			Console.WriteLine(answer);
 		}
